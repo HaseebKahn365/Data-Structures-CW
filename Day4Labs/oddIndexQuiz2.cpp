d970792cbb00db67537b0cf14ec64b44279ce4b1
HaseebKahn365 <58126830+HaseebKahn365@users.noreply.github.com>
2023-10-02 21:36:59 +0500

    Update oddIndexQuiz2.cpp

--- Day4Labs/oddIndexQuiz2.cpp
+++ Day4Labs/oddIndexQuiz2.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int avgCalculator(int* , int);
-int main(){
+int avgCalculator(int *, int);
+int main()
+{
 
-int array[5] = {2,2,3,4,6};
+    int array[5] = {2, 2, 3, 4, 6};
 
-int avg = avgCalculator(array, 5);
+    int avg = avgCalculator(array, 5);
 
-cout<<"The average of the odd integers is: "<< avg;
-
-
+    cout << "The average of the odd integers is: " << avg;
 }
 
-int avgCalculator(int* arr, int size){
+int avgCalculator(int *arr, int size)
+{
 
-    int sum=0;
+    int sum = 0;
 
-    for(int i=1; i<size; i+=2){
-        sum+= *(arr+i);
+    for (int i = 1; i < size; i += 2)
+    {
+        sum += *(arr + i);
     }
 
-    return sum/(size/2);
+    return sum / (size / 2);
 }
